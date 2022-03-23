d139df5cee66c963ed2e4fefc8d81ea5f5521b15
ahat92 <102114179+ahat92@users.noreply.github.com>
2022-03-23 09:20:47 -0400

    Update StartScreen.java

--- src/com/company/StartScreen.java
+++ src/com/company/StartScreen.java
@@ -1,7 +1,20 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class StartScreen extends JPanel {
+    JButton[] numPlayers = new JButton[2];
+    JPanel title, selection;
     public StartScreen(){
+
+        //TitleWord made
+        title = new JPanel();
+        JLabel titleWord = new JLabel("CHESS",SwingConstants.CENTER);
+        titleWord.setFont(new Font(Font.SERIF,Font.ITALIC, 100));
+        title.setLayout(new BorderLayout());
+        title.add(titleWord, BorderLayout.CENTER);
+        title.setBackground(GUI.background);
+
+        this.add(title);
 
     }
 }
